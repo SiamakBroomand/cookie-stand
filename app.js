4344dc6d4a9b47bc94c2312005729661cb623762
Siamak Broomand <broomand.siamk85@gmail.com>
2016-12-07 21:42:58 -0800

    made the event

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var storeList = document.getElementById('store-list');
+var storeForm = document.getElementById('store-form');
+var clearStoreList = document.getElementById('clear-store-list');
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
 var allStores = ['First and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
 var storeTable = document.getElementById('storejs');
 
@@ -28,19 +30,52 @@ var Store = function(locationName, minCustPerHour, maxCustPerHour, avgCookiesper
       this.totalDailyCookiesSales += this.totalCookiesSoldEachHour[i];
         // console.log(this.totalDailycookiesales, 'running total of daily cookie sales');
     }
-    Store.prototype.render = function(){
-      var trElStore = document.createElement('tr');
-      trElStore.innerHTML = this.locationName + this.minCustPerHour + this.maxCustPerHour + this.avgCookiesperCust;
-      return trElStore;
-    }
-    // function renderAllStores(){
-    //   storeRow.innerHTML= ' ';
-    //   for (var i = 0; i < allStores.length; i++){
-    //     storeRow.appendChild(allStores[i].render());
-    //   }
-    // }
   }
+  Store.prototype.render = function(){
+    var trElStore = document.createElement('tr');
+    trElStore.innerHTML = this.locationName + this.minCustPerHour + this.maxCustPerHour + this.avgCookiesperCust;
+    return trElStore;
+  }
+  function renderAllStores(){
+    storeList.innerHTML = ' ';
+    for (var i = 0; i < allStores.length; i++){
+      storeList.appendChild(allStores[i].render());
+    }
+  }
+  function handleCommentSubmit(event){
 
+    event.preventDefault(); //gotta have it for this purpose. prevents page reload on a 'submit' event
+    if (!event.target.store.value || !event.target.minCustomer.value || !event.target.maxCustomer.value || !event.target.averageCookies.value){
+      return alert('All fields must be filled!');
+    }
+
+    var storeName = event.target.store.value;
+    var minCust = event.target.minCustomer.value;
+    var maxCust = event.target.maxCustomer.value;
+    var avgCookie = event.target.averageCookies.value;
+
+    var newStore = new Store (storeName, minCust, maxCust, avgCookie);
+
+    event.target.store.value = null;
+    event.target.minCustomer.value = null;
+    event.target.maxCustomer.value = null;
+    event.target.averageCookies.value = null;
+    allStores.push(newStore);
+    renderAllStores();
+  }
+// // +++++++++++++++++++++++++++++++++++++++++++++++++++++
+// // Event listener for comment submission form
+  storeForm.addEventListener('submit', handleCommentSubmit);
+//
+// // +++++++++++++++++++++++++++++++++++++++++++++++++++++
+// // Event listener for the 'Clear all stores' button
+  clearStoreList.addEventListener('click', function() {
+    storeList.innerHTML = ' ';
+    // console.log('You just cleared the store list!');
+    allStores = [];
+  });
+
+  storeForm.addEventListener('submit', handleCommentSubmit);
 
   this.render = function(){
     this.calcTotalCookiesSoldEachHour();
